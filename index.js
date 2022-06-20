ac63e6925749b3affb6a16e495663ed93d955056
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-06-20 12:52:35 -0300

    fix: missuse of const variable

--- index.js
+++ index.js
@@ -86,17 +86,19 @@ app.post("/tweets", (req, res) => {
 app.get("/tweets", (req, res) => {
     console.log("GET request made to route /tweets");
     const pageQuery = req.query.page;
+    let page;
     if (pageQuery === undefined) {
         console.log("Pagination query not found, assuming page 1");
-        const page = 1;
+        page = 1;
     } else {
         console.log("Pagination query found, trying to convert to number");
-        const page = parseInt(pageQuery);
+        page = parseInt(pageQuery);
         if (isNaN(page) || page < 2) {
             console.log("Pagination is not a number or is lesser than 2");
             res.status(400);
             res.send("Informe uma página válida!");
             console.log("Response sent!");
+            return;
         }
     }
     const tweetsToSend = tweets.slice(10 * (page - 1), 10 * page);
